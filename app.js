2bf946b0ab7654ad6e10efc9fbc9ef1fb6f57e9a
nikoslos <28726212+nikoslos@users.noreply.github.com>
2021-10-29 17:50:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const { assert } = require('console');
 const io = new Server(server);
 
-const port = $PORT;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
